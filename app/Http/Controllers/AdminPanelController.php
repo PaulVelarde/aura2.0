b94b15192873100b91736d1a3122643f74962203
Paul Velarde <paulvelarde123a@gmail.com>
2024-12-12 00:59:57 -0400

    cambis vale

--- app/Http/Controllers/AdminPanelController.php
+++ app/Http/Controllers/AdminPanelController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Noticia;
+use App\Models\RedVideo;
 
 class AdminPanelController extends Controller
 {
@@ -13,5 +15,39 @@ class AdminPanelController extends Controller
            // dd("hola ya accedi");
             return view('admin.admin'); // Asegúrate de que admin.blade.php esté en la carpeta correcta
         }
+
+    public function storeNews(Request $request)
+    {
+        // Validar y procesar los datos del formulario de "Añadir Noticia"
+        $validatedData = $request->validate([
+            'titular' => 'required',
+            'contenido' => 'required',
+            'image' => 'required|image|max:2048',
+        ]);
+
+        $noticia = new Noticia();
+    $noticia->titular = $validatedData['titular'];
+    $noticia->contenido = $validatedData['contenido'];
+    $noticia->image = $request->file('image')->store('noticias', 'public');
+    
+    $noticia->save();
+
+        return redirect()->route('admin.index')->with('success', 'Noticia agregada correctamente.');
+    }
+
+    public function storeVideo(Request $request)
+    {
+        // Validar y procesar los datos del formulario de "Añadir Videos Redes Sociales"
+        $validatedData = $request->validate([
+            'titulo' => 'required',
+            'URL' => 'required|url',
+            'fuente' => 'required',
+        ]);
+
+        // Guardar el video en la base de datos
+        // ...
+
+        return redirect()->route('admin.index')->with('success', 'Video agregado correctamente.');
+    }
     
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\NoticiaController;
 use App\Http\Controllers\AdminPanelController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 // Rutas para noticias
 // Route::prefix('news')->name('news.')->group(function () {
@@ -11,8 +13,30 @@ use App\Http\Controllers\AdminPanelController;
 // });
 
 // Ruta para el panel de administración
-Route::get('/admin', [AdminPanelController::class, 'index'])->name('admin.index');
+//Route::get('/admin', [AdminPanelController::class, 'index'])->name('admin.index');
 
 // Rutas para la página de inicio
 //Route::get('/', [HomeController::class, 'loadAndRedirect'])->name('loading');
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+
+//admin
+Route::get('/admin', [AdminPanelController::class, 'index'])->name('admin.index');
+// routes/web.php
+Route::post('/admin/noticias', [AdminPanelController::class, 'storeNews'])->name('aura.store.news');
+Route::post('/admin/videos', [AdminPanelController::class, 'storeVideo'])->name('aura.store.video');
+
+
+
+//login 
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'store'])->name('login');
+Route::post('/logout', [AuthController::class, 'destroy'])->name('logout');
+Route::get('/registro', [AuthController::class, 'showRegistrationForm'])->name('registro');
+
+//pagina de aterrizaje 
+Route::get('/', function () {
+    return view('landing');
+})->name('landing');
+Route::post('/register', [UserController::class, 'store'])->name('register');
